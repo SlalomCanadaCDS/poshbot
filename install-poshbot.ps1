25dd890fbf6daf7c7cf3d9db0adc61c723710468
Ben Onucki <ben.onucki@slalom.com>
2018-07-24 09:07:50 -0400

    Initial commit

---
+++ install-poshbot.ps1
@@ -0,0 +1,39 @@
+﻿#Powershell script to install PoshBot as a service
+#Update Bot Name and Slack Token Variable before running
+$botname="poshbot"
+$botadmins="joe.beaudry","amin.teimoortagh"
+$slacktoken="asdfasdfasdf"
+$output = 'C:\'
+
+# Set Execution policy
+Set-ExecutionPolicy unrestricted
+
+# Install the module from PSGallery
+Install-Module -Name PoshBot -Repository PSGallery
+
+# Import the module
+Import-Module -Name PoshBot
+
+# Extract files
+
+Expand-Archive -Path poshbot.zip -DestinationPath  $output -force
+
+# Modify 'BotAdmins" & 'Token' before running script
+# Create bot configuration
+$botParams = @{
+    Name = "$botname"
+    BotAdmins = @($botadmins)
+    CommandPrefix = '!'
+    LogLevel = 'Info'
+    BackendConfiguration = @{
+        Name = 'SlackBackend'
+        Token = $slacktoken
+    }
+    AlternateCommandPrefixes = 'bender', 'hal'
+}
+
+$myBotConfig = New-PoshBotConfiguration @botParams
+Save-PoshBotConfiguration -InputObject $myBotConfig -Path C:\poshbot\Config.psd1 -force
+
+# Run PoshBot as a service
+#C:\poshbot\service-poshbot.ps1
